009408d8b592593efcc222119def70ee9fe0e8eb
satyamyt77629-stack <satya.myt77629@gmail.com>
2026-01-29 00:16:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const reveals = document.querySelectorAll(".reveal");
 
-window.addEventListener("scroll", () => {
-  reveals.forEach((el) => {
+function revealOnScroll() {
+  reveals.forEach(el => {
     const windowHeight = window.innerHeight;
     const elementTop = el.getBoundingClientRect().top;
-
     if (elementTop < windowHeight - 100) {
       el.classList.add("active");
     }
   });
-});
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
